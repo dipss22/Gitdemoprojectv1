7c38d4083e2fa5b0134b65f8b821fc8f9cf52fcf
hp <hp@LAPTOP-HPE3RD6N>
2022-06-21 12:28:13 +0530

    first commited

--- src/pack1/test1.java
+++ src/pack1/test1.java
@@ -2,4 +2,22 @@ package pack1;
 
 public class test1 {
 
+	public static void main(String[] args) {
+		
+	for(int i=1;i<=5;i++) 
+	{
+		for (int j=1;j<=5;j++)
+			System.out.print("*");
+		System.out.println(" ");
+	}
+		
+		
+		
+		
+	}
+
 }
+	
+	
+
+
